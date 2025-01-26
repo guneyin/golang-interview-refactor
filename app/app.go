97af0e5db81a2e238f001c6e6c6eb79b1d76acd8
Hüseyin Güney <guneyin@gmail.com>
2025-01-26 18:08:37 +0300

    custom recovery middleware implemented

--- app/app.go
+++ app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
@@ -9,6 +10,8 @@ import (
 	"interview/controllers"
 	"interview/mw"
 )
+
+var ErrInternalServerError = errors.New("internal Server Error")
 
 type Api struct {
 	port       string
@@ -23,7 +26,13 @@ func New(cfg config.AppConfig) *Api {
 	store := cookie.NewStore([]byte(cfg.SessionSecret))
 	store.Options(sessions.Options{MaxAge: 60 * 60 * 24})
 	router.Use(sessions.Sessions("session", store))
-	router.Use(gin.Recovery())
+	router.Use(gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
+		if err, ok := recovered.(error); ok {
+			mw.RenderError(c, fmt.Errorf("panic recovered: %v", err))
+		} else {
+			mw.RenderError(c, ErrInternalServerError)
+		}
+	}))
 	router.Use(mw.ErrorHandler())
 
 	return &Api{
@@ -35,4 +44,11 @@ func New(cfg config.AppConfig) *Api {
 
 func (a *Api) Start() error {
 	return a.router.Run(fmt.Sprintf(":%s", a.port))
+}
+
+func recoveryHandler(c *gin.Context, err interface{}) {
+	c.HTML(500, "error.tmpl", gin.H{
+		"title": "Error",
+		"err":   err,
+	})
 }
